3d60a318064d97560a349894d5d8c9f0b480edae
Aadil Mehraj <aadilmehraj76@gmail.com>
2020-11-01 12:19:53 +0530

    Add build scripts and docs

--- packages/utils/src/lib/text/escapeHtmlContent.ts
+++ packages/utils/src/lib/text/escapeHtmlContent.ts
@@ -1,9 +1,16 @@
+/**
+ * Convert the HTML content to plain text
+ *
+ * @export
+ * @param {string} [htmlContent] - actual html content to be escaped
+ * @returns the escaped html content
+ */
 export default function escapeHtmlContent(htmlContent?: string) {
   if (!htmlContent) return null;
 
   const escapeHtmlRegex = /(>|<)/g;
 
-  const matcher = (_, p1) => {
+  const matcher = (_: string, p1: string) => {
     if (p1 === '<') return '&lt;';
     if (p1 === '>') return '&gt;';
 
